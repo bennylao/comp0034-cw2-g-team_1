2dc8c11527a5d2acd82c54121b406608456b4eb8
NirvanBarua <111134129+NirvanBarua@users.noreply.github.com>
2023-03-04 17:36:38 +0000

    help page

--- crayfish_analysis_app/hello.py
+++ crayfish_analysis_app/hello.py
@@ -8,3 +8,9 @@ main_bp = Blueprint('main', __name__)
 @main_bp.route('/')
 def index():
     return render_template('index.html')
+
+@main_bp.route('/help', methods=['GET',])
+def help():
+    """Returns help page """
+    return 'Returns help page'
+
