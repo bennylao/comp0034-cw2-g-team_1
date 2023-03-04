d894e8bb03dd1998349d47e394e6e40d0fb307ed
Chon Hou Lao <zceslao@ucl.ac.uk>
2023-03-04 16:15:14 +0000

    liniting

--- crayfish_analysis_app/__init__.py
+++ crayfish_analysis_app/__init__.py
@@ -5,13 +5,9 @@ def create_app(config_class_name):
     """Create and configure the Flask app"""
     app = Flask(__name__)
 
+    app.config.from_object(config_class_name)
+
     from crayfish_analysis_app.hello import main_bp
     app.register_blueprint(main_bp)
-
-    app.config["SECRET_KEY"] = "123"
-    app.config.from_object(config_class_name)
-    # Include the routes from hello.py
-    with app.app_context():
-        from . import hello
 
     return app
