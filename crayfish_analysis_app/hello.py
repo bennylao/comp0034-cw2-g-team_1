cad6f434c1550e8b2ae51c4870041b0807952c3c
NirvanBarua <111134129+NirvanBarua@users.noreply.github.com>
2023-03-04 17:48:54 +0000

    some routes

--- crayfish_analysis_app/hello.py
+++ crayfish_analysis_app/hello.py
@@ -7,6 +7,7 @@ main_bp = Blueprint('main', __name__)
 
 @main_bp.route('/')
 def index():
+    """Returns home page """
     return render_template('index.html')
 
 @main_bp.route('/help', methods=['GET',])
@@ -14,3 +15,17 @@ def help():
     """Returns help page """
     return 'Returns help page'
 
+@main_bp.route('/about', methods=['GET',])
+def about():
+    """Returns about page """
+    return 'Returns about page'
+
+@main_bp.route('/forum', methods=['GET',])
+def forum():
+    """Returns forum page"""
+    return 'Returns forum page'
+
+@main_bp.route('/user/<username>')
+def user(username):
+    """Returns user account page """
+    return f"Returns account page for {username}"
