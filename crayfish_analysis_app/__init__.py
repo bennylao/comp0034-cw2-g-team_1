9cdf48dd22bde14a91691d402d2e339fc9d37f0b
NirvanBarua <111134129+NirvanBarua@users.noreply.github.com>
2023-04-01 01:51:08 +0100

    created sql database

--- crayfish_analysis_app/__init__.py
+++ crayfish_analysis_app/__init__.py
@@ -1,14 +1,22 @@
 from flask import Flask
-
+from .models import db
 
 def create_app(config_class_name):
     """Create and configure the Flask app"""
-    app = Flask(__name__,template_folder="templates")
+    app = Flask(__name__)
 
     app.config.from_object(config_class_name)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     from crayfish_analysis_app.auth import main_bp
     
     app.register_blueprint(main_bp, url_prefix="/")
+
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
+        print("Database created successfully!")
 
     return app
--- crayfish_analysis_app/auth.py
+++ crayfish_analysis_app/auth.py
@@ -1,21 +1,31 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from .models import User, db
 
 main_bp = Blueprint('auth', __name__)
 
 @main_bp.route('/')
-@main_bp.route('/home')
+@main_bp.route("/home")
 def home():
     """Returns home page """
     return render_template('home.html')
 
-@main_bp.route('/signup')
+@main_bp.route("/signup", methods=['GET','POST'])
 def signup():
-    """Returns signup page """
+    """Render signup page and handle signup form submission"""
+    email = request.form.get("email")
+    username = request.form.get("username")
+    password1 = request.form.get("password1")
+    password2 = request.form.get("password2")
+    # user = User(email=email, username=username, password=password1)
+    # db.session.add(user)
+    # db.session.commit()
     return render_template('signup.html')
 
-@main_bp.route('/login')
+@main_bp.route("/login", methods=['GET','POST'])
 def login():
     """Returns login page"""
+    email = request.form.get("email")
+    password = request.form.get("password")
     return render_template('login.html')
 
 # @main_bp.route('/about', methods=['GET',])
